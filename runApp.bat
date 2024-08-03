087d5cfe189d1a5837ae16ca85c94c8be2c90509
BortnikD <dima100421@gmail.com>
2024-08-03 14:13:11 +0300

    Подправлен runApp.bat

--- runApp.bat
+++ runApp.bat
@@ -6,4 +6,4 @@ call python manage.py migrate
 if %errorlevel% neq 0 exit /b %errorlevel%
 call python manage.py loaddata games/fixtures/gameType.json games/fixtures/game.json
 if %errorlevel% neq 0 exit /b %errorlevel%
-call python manage.py ru
+call python manage.py runserver
