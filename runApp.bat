642950d5fb738af383a1fac3887b33fe15a8fd62
BortnikD <dima100421@gmail.com>
2024-08-03 13:59:45 +0300

    Подправлен runApp.bat и в readme подправлена команда запуска скрипта runApp.bat

--- runApp.bat
+++ runApp.bat
@@ -1,9 +1,10 @@
 @echo off
-cd main
+cd GameList
 call pip install -r requirements.txt
 if %errorlevel% neq 0 exit /b %errorlevel%
+cd main
 call python manage.py migrate
 if %errorlevel% neq 0 exit /b %errorlevel%
 call python manage.py loaddata games/fixtures/gameType.json games/fixtures/game.json
 if %errorlevel% neq 0 exit /b %errorlevel%
-call python manage.py runserver
+call python manage.py ru
